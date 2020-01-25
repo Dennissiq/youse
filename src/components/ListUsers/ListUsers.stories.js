b4766aa4f4ed36a28e545583ed9cae907e0830ed
Dennis <ti.dennissiqueira@gmail.com>
2020-01-25 08:42:01 -0300

    refactor: add decorator in redux provider

--- src/components/ListUsers/ListUsers.stories.js
+++ src/components/ListUsers/ListUsers.stories.js
@@ -1,6 +1,9 @@
 import React from 'react'
 import { storiesOf } from '@storybook/react'
 import { withInfo } from '@storybook/addon-info'
+import Provider from '../../utils/provider'
+
+import store from '../../store'
 
 import ListUsers from './ListUsers'
 
@@ -8,9 +11,13 @@ export const dataItems = [{}]
 
 export const searchContent = {}
 
-storiesOf('List', module).add(
-  'List Users',
-  withInfo()(() => (
-    <ListUsers items={dataItems} searchContent={searchContent} />
-  ))
-)
+const withProvider = story => <Provider store={store}>{story()}</Provider>
+
+storiesOf('List', module)
+  .addDecorator(withProvider)
+  .add(
+    'List Users',
+    withInfo()(() => (
+      <ListUsers items={dataItems} searchContent={searchContent} />
+    ))
+  )
